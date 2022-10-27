c799c8093bb8dd3e0edcc1f6a60697e08bf51f21
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-10-27 17:00:45 +0100

    Center gallery, overflow hidden, add buttons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import "./style.css";
-import { populateImages } from "./populateImages.js"
+import { populateImages } from "./populateImages.js";
+import firstImage from "./Group 5.png"
+import secondImage from "./Group 14 (1).png"
+import thirdImage from "./Hydra Motorway Photo.png"
 
 populateImages();
+
+const imageOne = document.getElementById("image0");
+const imageTwo = document.getElementById("image1");
+const imageThree = document.getElementById("image2");
+
+imageOne.src = firstImage;
+imageTwo.src = thirdImage;
+
 
