27ba945061024282c3b8269a64a853ea8b68c2fa
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-10-27 17:10:15 +0100

    Add event listners to add and remove classes to make slider slide

--- src/index.js
+++ src/index.js
@@ -9,8 +9,21 @@ populateImages();
 const imageOne = document.getElementById("image0");
 const imageTwo = document.getElementById("image1");
 const imageThree = document.getElementById("image2");
+const rightButton = document.getElementById("rightButton");
+const leftButton = document.getElementById("leftButton");
+const superLongDiv = document.querySelector(".superLongDiv");
 
 imageOne.src = firstImage;
 imageTwo.src = thirdImage;
+
+rightButton.addEventListener("click", (e) => {
+  superLongDiv.classList.remove("slideRight")
+    superLongDiv.classList.add("slideLeft");
+})
+
+leftButton.addEventListener("click", (e) => {
+    superLongDiv.classList.remove("slideLeft")
+     superLongDiv.classList.add("slideRight");
+})
 
 
